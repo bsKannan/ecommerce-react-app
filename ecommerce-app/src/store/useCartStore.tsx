25c8618cd64a665b7bbad03643e120c8b761fb51
Kannan B S <bskannankanna@gmail.com>
2026-03-29 22:22:10 +0530

    cart ui will updated and persist to use refresh the page atc products will not cleared

--- ecommerce-app/src/store/useCartStore.tsx
+++ ecommerce-app/src/store/useCartStore.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 export type Product = {
     id: number;
     title: string;
@@ -13,14 +14,22 @@ type CartStore = {
     removeFromCart: (id: number) => void;
     clearCart: () => void;
 };
+// Create a Zustand store for the shopping cart with persistence
 
-export const useCartStore = create<CartStore>((set)=> ({
+export const useCartStore = create<CartStore>()(
+  persist(
+  (set)=> ({
     cartItems: [],
   
     addToCart: (product) =>
-    set((state) => ({
-      cartItems: [...state.cartItems, product],
-    })),
+    set((state) => {
+      const exists = state.cartItems.find((item)=> item.id === product.id);
+      
+      if(exists) return state; // Prevent duplicates
+      
+      return { cartItems: [...state.cartItems, product]};
+
+    }),
 
   removeFromCart: (id) =>
     set((state) => ({
@@ -32,4 +41,8 @@ export const useCartStore = create<CartStore>((set)=> ({
         cartItems: [],
     })),
     
-}))
+}),
+{
+    name: "cart-storage",
+}
+));
