866f6ceb6a994a99eaea26b2f1e134d597db7856
Kannan B S <bskannankanna@gmail.com>
2026-03-29 23:01:32 +0530

    increment and decrement is added in cart page

--- ecommerce-app/src/store/useCartStore.tsx
+++ ecommerce-app/src/store/useCartStore.tsx
@@ -7,10 +7,13 @@ export type Product = {
     image: string;
     category: string;
     description: string;
+    quantity?: number; // Optional quantity field for cart items
 };
 type CartStore = {
     cartItems: Product[];
-    addToCart: (product:Product)=> void;
+    addToCart: (product:Omit<Product,"quantity">)=> void;
+    increaseQuantity: (id: number) => void;
+    decreaseQuantity: (id: number) => void;
     removeFromCart: (id: number) => void;
     clearCart: () => void;
 };
@@ -25,11 +28,37 @@ export const useCartStore = create<CartStore>()(
     set((state) => {
       const exists = state.cartItems.find((item)=> item.id === product.id);
       
-      if(exists) return state; // Prevent duplicates
+      if(exists) {
+        return { 
+          cartItems: state.cartItems.map((item)=>
+          item.id === product.id
+        ? {...item, quantity: (item.quantity ?? 1)+1}
+        : item) 
+      };
+      }
       
-      return { cartItems: [...state.cartItems, product]};
+      return {
+        cartItems: [...state.cartItems, {...product, quantity: 1}]
+      }
 
     }),
+
+    increaseQuantity: (id) =>
+      set((state) => ({
+        cartItems: state.cartItems.map((item) =>
+          item.id === id ? { ...item, quantity: (item?.quantity ?? 1) + 1 } 
+        : item
+        ),
+      })),
+
+    decreaseQuantity: (id) =>
+        set((state)=> ({
+          cartItems: state.cartItems.map((item)=>
+          item.id === id 
+          ? { ...item, quantity: (item.quantity ?? 1) - 1 } 
+          : item)
+          .filter((item)=> item.quantity && item.quantity > 0),
+        })),
 
   removeFromCart: (id) =>
     set((state) => ({
